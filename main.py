cd30cfe3313c04b271b63d000b62bc036b67d3b9
Ryuuz02 <ryuuz02@outlook.com>
2021-12-16 16:49:01 -0600

    Added Spells and magic (currently only player can use)

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class being:
         self.max_mana = mana
         self.equipment_lst = []
         self.speed = speed
+        self.spell_lst = []
 
     # Adds the stats of a gear if one of that type is not already equipped
     def equip_gear(self, equipped_gear):
@@ -88,6 +89,32 @@ class being:
                 print("You have been slain by " + self.name)
             opponent.alive = False
 
+    # casts a spell
+    def cast_spell(self, opponent):
+        # Prints out all spells the player has
+        print("Spells:")
+        for i in range(0, len(self.spell_lst)):
+            print(" " + str(i) + ": " + self.spell_lst[i])
+        # Asks user which one to cast
+        spell_choice = input("What spell would you like to cast (type the number tied to the spell), or "
+                             "type 'e' to exit\n")
+        # If the user wishes to back out of casting a spell
+        if spell_choice == "e":
+            self.take_action(opponent)
+        # Otherwise
+        else:
+            # Makes sure the user properly inputs an integer
+            try:
+                # If it is in the spell list
+                if int(spell_choice) < len(self.spell_lst):
+                    # Casts the spell
+                    cast_spell(self, opponent, self.spell_lst[int(spell_choice)])
+                # Else, will tell the user
+                else:
+                    print("You don't have that many spells")
+            except ValueError:
+                print("Not a valid input")
+
     # Each turn, all combatants will take their action
     def take_action(self, opponent):
         # Check to make sure they are alive
@@ -97,7 +124,7 @@ class being:
                 # Gives them an action prompt
                 action_choice = input("What would you like to do? \n"
                                       " 1: Attack your opponent\n"
-                                      " 2: Use magic(WIP)\n"
+                                      " 2: Use magic\n"
                                       " 3: Defend(WIP)\n"
                                       " 4: Run away(WIP)\n"
                                       " 5: Inspect your foe\n")
@@ -107,7 +134,8 @@ class being:
                     # 1 is attack
                     self.attack(opponent)
                 elif action_choice == "2":
-                    print("That is currently a Work in Progress")
+                    # 2 is cast spell
+                    self.cast_spell(opponent)
                 elif action_choice == "3":
                     print("That is currently a Work in Progress")
                 elif action_choice == "4":
@@ -131,6 +159,9 @@ class player(being):
     def __init__(self, health, armor, evasion, damage, mana, speed):
         super().__init__(health, armor, evasion, damage, mana, speed)
         self.name = "Player"
+        self.spell_lst.append("Fireball")
+        self.spell_lst.append("Heal")
+        self.spell_lst.append("Icicle")
 
 
 # Class for all enemies
@@ -145,6 +176,35 @@ class enemy(being):
             print(self.name + " with " + self.equipment_to_string())
         else:
             print(self.name)
+
+
+# Class for spells
+class spell:
+    # Assigning variables
+    def __init__(self, mana, spelltype, tier, name):
+        self.mana = mana
+        self.spelltype = spelltype
+        self.tier = tier
+        self.name = name
+
+    # function to actually cast itself
+    def cast_spell(self, user, opponent):
+        # If the user has enough mana
+        if user.mana >= self.mana:
+            # Takes their mana
+            user.mana -= self.mana
+            # Tells the user
+            print(user.name + " used " + str(self.mana) + " mana to cast " + self.name)
+            # Casts the spell
+            spell_function_dict[self.name](user, opponent)
+        # If the user does not have the mana
+        else:
+            # If the user is the player, tells them they don't have enough
+            if type(user) == player:
+                print("You do not have enough mana to use that")
+            # Otherwise tells them that someone else failed to cast
+            else:
+                print(user.name + " tried to cast " + self.name + " but did not have the mana for it")
 
 
 # Master list of all equipment
@@ -272,13 +332,66 @@ def create_goblin_mage():
     return goblin_mage
 
 
-# !-----Reminder for all create functions, add their function and name to monster_function_list and monster_lst-------!
-# !-----Reminder for all create functions, add their function and name to monster_function_list and monster_lst-------!
-# !-----Reminder for all create functions, add their function and name to monster_function_list and monster_lst-------!
+# !-----Reminder for all create functions, add their function and name to monster_function_dict and monster_lst-------!
+# !-----Reminder for all create functions, add their function and name to monster_function_dict and monster_lst-------!
+# !-----Reminder for all create functions, add their function and name to monster_function_dict and monster_lst-------!
 # Automates the process of creating enemies using the list and dictionary
 monster_lst = ["Goblin", "Orc", "Wolf", "Dude", "Goblin Mage"]
 monster_function_dict = {"Goblin": create_goblin, "Orc": create_orc, "Wolf": create_wolf, "Dude": create_dude,
                          "Goblin Mage": create_goblin_mage}
+
+
+# Functions for spells
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_lst-------!
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_lst-------!
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_lst-------!
+# Fireball Spell
+def fireball(user, opponent):
+    # Does damage equal to 1.5 times caster damage
+    inflicted_damage = user.damage * 1.5
+    opponent.health -= inflicted_damage
+    # Tells the user
+    print(user.name + " hurled a fireball at " + opponent.name + " for " + str(inflicted_damage) + " damage")
+
+
+# Heal spell
+def heal(user, opponent):
+    # Heals the caster for their damage plus a tenth of their max hp
+    health_heal = user.damage + user.max_health / 10
+    user.health += health_heal
+    # Tells the user
+    print(user.name + " healed themselves for " + str(health_heal) + " health")
+
+
+# Icicle spell
+def icicle(user, opponent):
+    # Does damage equal to caster's damage
+    inflicted_damage = user.damage
+    opponent.health -= inflicted_damage
+    # Reduces opponents speed by 10%
+    opponent.speed *= 0.9
+    # Tells the user
+    print(user.name + " dropped an icicle on " + opponent.name + " for " + str(inflicted_damage) + " damage, they were "
+                                                                                                   "also slowed by 10%")
+
+
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_class_dict-----!
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_class_dict-----!
+# !-----Reminder for all create functions, add their function and name to spell_function_dict and spell_class_dict-----!
+# Automates the process of creating spells and casting them using the 2 dictionaries
+# Classes for each spell, has further information about them
+Fireball = spell(5, "offense", 1, "Fireball")
+Heal = spell(10, "defense", 1, "Heal")
+Icicle = spell(5, "offense", 1, "Icicle")
+# One dictionary that has every class
+spell_class_dict = {"Fireball": Fireball, "Heal": Heal, "Icicle": Icicle}
+# One dictionary that has all the functions
+spell_function_dict = {"Fireball": fireball, "Heal": heal, "Icicle": icicle}
+
+
+# Casts the spell
+def cast_spell(user, opponent, spell_name):
+    spell_class_dict[spell_name].cast_spell(user, opponent)
 
 
 # Randomly chooses a piece of gear from the equipment list
